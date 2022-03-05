3318f7e4bf9bda854ad0a638afdc72492433d797
Lejun Chen <chenlejun@gmail.com>
2022-03-05 23:26:43 +0100

    modify the filenames

--- calculator_3/calculation_noParentheses.go
+++ calculator_3/calculation_noParentheses.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"os"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-func clientLoop() {
-	var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
-
-	//wait for the input from the user and print result
-	for {
-		var operators  []string
-		var numbers  []float64
-		fmt.Println("Enter expression e.g. 6*8+(7-8/9+6)*(6+9*8):")
-		scanner.Scan()
-		expression := scanner.Text()
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Error reading text:", err)
-		}
-		// for _, charRune := range expression {
-		// 	char := string(charRune)
-		// 	fmt.Println("char:", char)
-		// }
-
-		numbers=extractNumbers(expression)
-		operators=extractOperators(expression)
-		numbersNew, operatorsNew:=removeAllParentheses(numbers, operators)
-		result:=noParenthesesCalculation(numbersNew, operatorsNew)
-		fmt.Println("The result of expression ",expression," is:",result)
-
-
-	}
-}
 
 func extractOperators(expression string) []string{
 	var operators  []string
